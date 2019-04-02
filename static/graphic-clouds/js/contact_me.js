b13d3cb49cad435e846b6a81d4a76542fe8c5b41
aejiou <khandro.an@gmail.com>
2019-04-02 15:00:53 -0700

    actual page, basic flask server, mock backend frontend connection

---
+++ static/graphic-clouds/js/contact_me.js
@@ -0,0 +1,71 @@
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      unique_id();
+      write_message('Information submitted! Starting...','success');
+      my_data = sendStyle();
+
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $.ajax({
+        url: "/clouds-submit",
+        type: "POST",
+        data: my_data,
+        cache: false,
+        success: function() {
+          // Success message
+          write_message('Request completed successfully!','success');
+          //clear all fields
+          //$('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          write_message('Sorry, something is wrong with the server. Please try again later!','danger');
+          //clear all fields
+          //$('#contactForm').trigger("reset");
+        },
+        complete: function(jqXHR,status) {
+          if (status == 'success')
+          {
+            write_output(JSON.parse(jqXHR.responseText));
+            alert(my_obj);
+            setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+          }
+
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  alert(1);
+  $('#progress').html('');
+});
---
+++ static/graphic-clouds/js/custom.js
@@ -0,0 +1,54 @@
+function imToggle(im,src)
+{
+  document.getElementById(im).src = "img/about/"+src+".jpg";
+}
+
+function sendStyle()
+{
+    var elems = ['identifier','name','engine','t_im_range','transform','colors','im_width','im_height'];
+    var checkboxes = ['font1','font2','font3','font4','inverted'];
+    var dt = "var data = { ";
+    for (i=0; i<elems.length; i++)
+     {
+         dt = dt + '"' + elems[i] + '" : "' + document.getElementById(elems[i]).value + '"' + ' , ';
+     }
+    for (i=0; i<checkboxes.length; i++)
+     {
+         dt = dt + '"' + checkboxes[i] + '" : "' + document.getElementById(checkboxes[i]).checked + '"' + ((i == checkboxes.length-1) ? '' : ' , ');
+     }
+    dt = dt + " }";
+    d = eval(dt);
+    return data;
+
+}
+
+
+function write_message(msg,t)
+ {
+    $('#progress').html("<div class='alert alert-"+t+"'>");
+    //$('#progress > .alert-'+t).html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+    //  .append("</button>");
+    $('#progress > .alert-'+t)
+      .append("<strong>"+msg+"</strong>");
+    $('#progress > .alert-'+t)
+      .append('</div>');
+
+ }
+
+function unique_id()
+ {
+     r = "";
+     for (i=0; i<16; i++)
+      {
+        r = r + String.fromCharCode(Math.round(Math.random()*22)+65);
+      }
+    document.getElementById('identifier').value = r;
+
+ }
+
+function write_output(result)
+ {
+    $('#output').html("<h2>"+result.concept+"</h2>");
+    $('#output').append('    <img class="img-fluid d-block mx-auto" src="'+result.src+'" alt="">');
+    $('#output').append('<button class="btn btn-primary" data-dismiss="modal" type="button"><i class="fas fa-times"></i>Close</button>');
+ }
